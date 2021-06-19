f1997149855848025c3c2345497bb1fdb9d8fa0a
endodaiki <82555115+0925Daiki@users.noreply.github.com>
2021-06-19 12:53:22 +0900

    6/19trainig

--- memo/input_do.php
+++ memo/input_do.php
@@ -23,7 +23,10 @@
 try{
   $db = new PDO('mysql:dbname=my_db;host=127.0.0.1;charset=utf8','root','');
 
-  $db -> exec('INSERT INTO memos SET memo="' . $_POST['memo'] . '", created_at=NOW()');
+  $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+  $statement->bindParam(1,$_POST['memo']);
+  $statement->execute(array($_POST['memo']));
+  echo 'メッセージが登録されました';
 } catch(PDOException $e){
   echo 'DB接続エラー:' . $e->getMessage();
 }
