1b9da9d7c871044cbb8d920363910ba991f5d7f1
endodaiki <82555115+0925Daiki@users.noreply.github.com>
2021-06-21 09:56:19 +0900

    6/21trainig メッセージ返信機能

--- MINI_BBS/index.php
+++ MINI_BBS/index.php
@@ -15,8 +15,8 @@ if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
 //投稿するボタンがクリックされた時(メッセージが空でなければ)
 if (!empty($_POST)){
   if($_POST['message'] !== ''){
-    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created = NOW()');
-    $message->execute(array($member['id'],$_POST['message']));
+    $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created = NOW()');
+    $message->execute(array($member['id'],$_POST['message'],$_POST['reply_post_id']));
 
     //再読み込みしても情報が重複しないようにする
     header('Location: index.php');
@@ -25,6 +25,15 @@ if (!empty($_POST)){
 }
 //投稿を「取得する」 (m/p→membersテーブルの省略形 p*=postsテーブルのすべての値 p.created DESC→日付順に降順で並ばせる)
 $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+if(isset($_REQUEST['res'])){
+  //返信の処理
+  $response = $db->prepare('SELECT m.name,m.picture, p.* FROM members m,posts p WHERE m.id=p.member_id AND p.id=?');
+  $response->execute(array($_REQUEST['res']));
+
+  $table = $response->fetch();
+  $message = '@' . $table['name'] . ' ' . $table['message'];
+}
 
 ?>
 
@@ -50,8 +59,8 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
       <dl>
         <dt><?php print(htmlspecialchars($member['name'],ENT_QUOTES));?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'],ENT_QUOTES));?>" />
         </dd>
       </dl>
       <div>
@@ -67,7 +76,7 @@ $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE
 foreach($posts as $post):?>
     <div class="msg">
     <img src="member_picture/<?php print(htmlspecialchars($post['picture'],ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'],ENT_QUOTES)); ?>" />
-    <p><?php print(htmlspecialchars($post['message'],ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES)); ?>）</span>[<a href="index.php?res=">Re</a>]</p>
+    <p><?php print(htmlspecialchars($post['message'],ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'],ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'],ENT_QUOTES)); ?>">Re</a>]</p>
     <p class="day"><a href="view.php?id="><?php print(htmlspecialchars($post['created'],ENT_QUOTES)); ?></a>
 <a href="view.php?id=">
 返信元のメッセージ</a>
